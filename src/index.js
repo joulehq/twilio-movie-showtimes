5fdb9586670c543b00bdca218f489d00014fb72c
Jaisen Mathai <jaisen@jmathai.com>
2016-03-08 05:09:33 -0800

    Add additional endpoints

--- src/index.js
+++ src/index.js
@@ -11,24 +11,42 @@
  */
 var Response = require('joule-node-response');
 var Showtimes = require('./showtimes');
+var twilio = require('twilio');
+var TwimlResponse = require('twilio').TwimlResponse;
 
 var CONST = {
   initialState: 'start',
   timeout: 10,
   finishOnKey: '#',
-  baseUrl: 'https://api.joule.run/jmathai/twilio-movie-showtimes'
+  baseUrl: 'https://api.joule.run/jmathai/twilio-movie-showtimes',
+  twimlSayOptions: {
+      voice:'woman'
+  }
 };
 
+
 var start = function(event, response) {
-  response.send('<?xml version="1.0" encoding="UTF-8"?> <Response> <Gather timeout="10" finishOnKey="#" action="'+CONST.baseUrl+'/greet"> <Say voice="alice">Hello, I\'m Rachel. Enter your zipcode and I will look up movie showtimes for you.</Say> </Gather> </Response>');
+  var twimlResponse = new TwimlResponse();
+
+  twimlResponse.gather({
+      finishOnKey: '#',
+      action: CONST.baseUrl+'/greet'
+    }, function() {
+      this.say('Hello, I\'m Rachel. Enter your zipcode and I will look up movie showtimes for you.', CONST.twimlSayOptions);
+    }
+  );
+  response.send(twimlResponse.toString());
 };
 
 var greet = function(event, response) {
-  var showtimes = new Showtimes(event.post['Digits'], {});
+  var showtimes = new Showtimes(event.post['Digits'], {})
+      , twimlResponse = new TwimlResponse();
   showtimes.getTheaters(function(err, theaters) {
     if(err) {
       console.log(err);
-      response.send('<?xml version="1.0" encoding="UTF-8"?><Response><Say voice="alice">Oh no! I don\'t know what happened.</Say></Response>');
+      twimlResponse.say('Oh no! I don\'t know what happened. I\'ll redirect you to the main menu.', CONST.twimlSayOptions);
+      response.send(twimlResponse.toString());
+      return;
     }
     
     var res = [];
@@ -39,16 +57,85 @@ var greet = function(event, response) {
       res.push({id: theaters[i].id, name: theaters[i].name});
     }
 
-    var out = '<?xml version="1.0" encoding="UTF-8"?><Response><Say voice="alice">Looks like I found ' + theaters.length + ' theaters in your area. Here are the top ' + res.length + '.</Say>';
-    for(var j in res) {
-      out += '<Say voice="alice">' + res[j].name.replace(/[^0-9a-zA-Z ]/, '') + '</Say>';
-      if(j <= res.length-1) {
-        out += '<Pause length="1"/>';
+    twimlResponse.say('Looks like I found ' + theaters.length + ' theaters in your area. Here are the top ' + res.length + '.', CONST.twimlSayOptions);
+    twimlResponse.gather({
+      finishOnKey: '#',
+      action: CONST.baseUrl+'/movies?zipcode='+event.post['Digits']
+    }, function() {
+      var counter = 1;
+      for(var j in res) {
+        this.say('Press ' + counter + ' for ' + res[j].name, CONST.twimlSayOptions);
+        if(j <= res.length-1) {
+          this.pause({length: 1});
+        }
+        counter++;
+      }
+    });
+
+    response.send(twimlResponse.toString());
+  });
+};
+
+var movies = function(event, response) {
+  var showtimes = new Showtimes(event.query['zipcode'], {})
+      , twimlResponse = new TwimlResponse();
+  showtimes.getTheaters(function(err, theaters) {
+    var movies
+        , theater;
+    if(err) {
+      console.log(err);
+      twimlResponse.say('Oh no! I don\'t know what happened. I\'ll redirect you to the main menu.', CONST.twimlSayOptions);
+      response.send(twimlResponse.toString());
+      return;
+    }
+    
+    var counter = 1;
+    for(var i in theaters) {
+      if(counter == event.post['Digits']) {
+        theater = theaters[i];
+        movies = theater['movies'];
+        break;
+      }
+      counter++;
+    }
+
+    twimlResponse.say('I found ' + movies.length + ' movies playing at ' + theater.name + '. You can press the movie\'s number at any time and I\'ll send you a link to purchase tickets.', CONST.twimlSayOptions);
+    twimlResponse.gather({
+      finishOnKey: '#',
+      action: CONST.baseUrl+'/showtimes?theater='+theater.id
+    }, function() {
+      var counter = 1;
+      for(var j in movies) {
+        this.say('Press ' + counter + ' for ' + movies[i].name, CONST.twimlSayOptions);
+        if(j <= movies.length-1) {
+          this.pause({length: 1});
+        }
+        counter++;
+      }
+    });
+
+    response.send(twimlResponse.toString());
+  });
+
+};
+
+var showtimes = function(event, response) {
+  var movie
+      , twimlResponse = new TwimlResponse();
+
+
+  showtimes.getTheater(event.query['theater'], function(err, theater) {
+    var movies = theater.movies
+        , movie
+        , showtimes
+        , counter = 1;
+    for(var i in movies) {
+      if(counter == event.post['Digits']) {
+        movie = movies[i];
+        showtimes = movie.showtimes;
+        break;
       }
     }
-    out += '</Response>';
-
-    response.send(out);
   });
 };
 
@@ -67,7 +154,15 @@ exports.handler = function(event, context) {
   switch(thisState) {
     case 'start':
       start(event, response);
+      break;
     case 'greet':
       greet(event, response);
+      break;
+    case 'movies':
+      movies(event, response);
+      break;
+    case 'showtimes':
+      sms(event, response);
+      break;
   }
 };
